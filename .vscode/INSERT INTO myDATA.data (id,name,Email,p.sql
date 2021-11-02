723003ebf79febedf0dc13b0eb037277f8388190
[bhargav dharan] <bhargavdharan20@gmail.com>
2021-11-02 20:14:19 +0530

    Added

---
+++ .vscode/INSERT INTO myDATA.data (id,name,Email,p.sql
@@ -0,0 +1,6 @@
+INSERT INTO myDATA.data (id,name,Email,phone) VALUES(
+    1,
+    'Dharan',
+    'beradharan@gmail.com',
+    735845
+)
---
+++ postgreSQL.session.sql
@@ -0,0 +1,9 @@
+-- CREATE TABLE data(id int,name varchar(255),email varchar(255))
+INSERT INTO data (id, name, email)
+VALUES 
+( 1,'Dharan','beradharan@gmail.com' ),
+( 2,'Srinu','sri@gmail.com' ),
+( 3,'Varun','varun@gmail.com' ),
+( 4,'Aanand','Aanand@gmail.com' ),
+( 5,'Ravi','Tej@gmail.com' )
+;
